c5a8d7f00c24854c9b20d21306b6a40519fdf324
Nikita Deepak Poyrekar <nikita.poyarekar5@gmail.com>
2025-06-05 10:48:44 +0530

    Update code for local setup

--- dist/main.js
+++ dist/main.js
@@ -2,11 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app/app.module");
+const dotenv = require("dotenv");
+dotenv.config();
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
-    await app.listen(process.env.PORT ?? 3300);
+    await app.listen(process.env.PORT ?? 3300, '0.0.0.0');
     console.log(`Application is running on: ${await app.getUrl()}`);
+    console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3300, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 }
 bootstrap();
