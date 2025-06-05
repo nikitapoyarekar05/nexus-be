a325a892a710904dbe97db2bc18ea5767ac096c3
Nikita Deepak Poyrekar <nikita.poyarekar5@gmail.com>
2025-06-05 08:20:53 +0530

    Revert port updates

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-
-  const port = process.env.PORT ?? 3300;
-  await app.listen(port, '0.0.0.0'); // ← Bind to all network interfaces!
-
-  console.log(`🚀 Application is running on: ${await app.getUrl()}`);
+  await app.listen(process.env.PORT ?? 3300);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
