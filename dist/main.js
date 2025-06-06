e67d96ef2f0cea7390726c2bda4668ef49db94a8
Nikita Deepak Poyrekar <nikita.poyarekar5@gmail.com>
2025-06-06 12:58:13 +0530

    Refactor code for hiding env variables

--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app/app.module");
-const dotenv = require("dotenv");
-dotenv.config();
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
     await app.listen(process.env.PORT ?? 3300, '0.0.0.0');
     console.log(`Application is running on: ${await app.getUrl()}`);
-    console.log('DB_USERNAME:', process.env.DB_USERNAME);
-    console.log('DB_NAME:', process.env.DB_NAME);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3300, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
-  console.log('DB_USERNAME:', process.env.DB_USERNAME);
-  console.log('DB_NAME:', process.env.DB_NAME);
+  // console.log('process.env.DB_USERNAME:', process.env.DB_USERNAME);
 }
 bootstrap();
