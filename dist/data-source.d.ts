80db8ced9a57e39a22551269ec9905977886c521
Nikita Deepak Poyrekar <nikita.poyarekar5@gmail.com>
2025-10-24 19:28:32 -0500

    Remove dist folder

--- dist/data-source.d.ts
+++
@@ -1,3 +0,0 @@
-import 'dotenv/config';
-import { DataSource } from 'typeorm';
-export declare const AppDataSource: DataSource;
--- dist/listing/listing/listing.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Listing = void 0;
-class Listing {
-}
-exports.Listing = Listing;
-//# sourceMappingURL=listing.js.map
